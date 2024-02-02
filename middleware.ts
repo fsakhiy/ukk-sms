a0723405b5cefa6ee843cdd268f3bb38c9a29fe0
Fairuz Sakhiy <sakhiyfairuz@gmail.com>
2024-02-02 09:31:45 +0700

    removed unused code

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,3 @@
-// import { NextResponse } from 'next/server'
-// import type { NextRequest } from 'next/server'
-//
-// // This function can be marked `async` if using `await` inside
-// export function middleware(request: NextRequest) {
-//     return NextResponse.redirect(new URL('/home', request.url))
-// }
-//
-// // See "Matching Paths" below to learn more
-// export const config = {
-//     matcher: '/about/:path*',
-// }
-
 import {NextRequest} from "next/server";
 
 export { default } from "next-auth/middleware"
