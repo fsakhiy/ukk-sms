7a3aba7e65cd8c5711b5a5d832dc1e36148c3aa6
Fairuz Sakhiy <sakhiyfairuz@gmail.com>
2024-02-04 18:48:32 +0700

    modified styling of the datatable

--- components/web-component/DataTable.tsx
+++ components/web-component/DataTable.tsx
@@ -61,7 +61,8 @@ export function DataTable<TData, TValue>({
     }
 
     return (
-        <div className="rounded-md border">
+    <div className={'space-y-5'}>
+        <div className="rounded-md border ">
             <Table>
                 <TableHeader>
                     {table.getHeaderGroups().map((headerGroup) => (
@@ -104,39 +105,40 @@ export function DataTable<TData, TValue>({
                     )}
                 </TableBody>
             </Table>
-            <div className={'flex p-3'}>
-                <div className="flex-1 text-sm text-muted-foreground justify-normal items-center">
-                    {table.getFilteredSelectedRowModel().rows.length} of{" "}
-                    {table.getFilteredRowModel().rows.length} row(s) selected.
-                </div>
-                {/*<div className={'justify-end items-center'}>*/}
-                    <div className="flex items-center justify-end space-x-2">
-                        <Button
-                            variant="outline"
-                            size="sm"
-                            onClick={() => table.previousPage()}
-                            disabled={!table.getCanPreviousPage()}
-                        >
-                            Previous
-                        </Button>
-                        <Button
-                            variant="outline"
-                            size="sm"
-                            onClick={() => table.nextPage()}
-                            disabled={!table.getCanNextPage()}
-                        >
-                            Next
-                        </Button>
-                        <Button
-                            variant={'destructive'}
-                            size={'sm'}
-                            onClick={handleDataDeletion}
-                            >
-                            delete
-                        </Button>
-                    </div>
-                {/*</div>*/}
-            </div>
         </div>
-    )
+        <div className={'flex'}>
+            <div className="flex-1 text-sm text-muted-foreground justify-normal items-center">
+                {table.getFilteredSelectedRowModel().rows.length} of{" "}
+                {table.getFilteredRowModel().rows.length} row(s) selected.
+            </div>
+            {/*<div className={'justify-end items-center'}>*/}
+            <div className="flex items-center justify-end space-x-2">
+                <Button
+                    variant="outline"
+                    size="sm"
+                    onClick={() => table.previousPage()}
+                    disabled={!table.getCanPreviousPage()}
+                >
+                    Previous
+                </Button>
+                <Button
+                    variant="outline"
+                    size="sm"
+                    onClick={() => table.nextPage()}
+                    disabled={!table.getCanNextPage()}
+                >
+                    Next
+                </Button>
+                <Button
+                    variant={'destructive'}
+                    size={'sm'}
+                    onClick={handleDataDeletion}
+                >
+                    delete
+                </Button>
+            </div>
+            {/*</div>*/}
+        </div>
+    </div>
+)
 }
