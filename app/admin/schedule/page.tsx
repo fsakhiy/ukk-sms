cb69095f3131b0c48444ecb04d733ce196f30e46
Fairuz Sakhiy <sakhiyfairuz@gmail.com>
2024-02-04 20:24:14 +0700

    added new schedule page

---
+++ app/admin/schedule/page.tsx
@@ -0,0 +1,14 @@
+export default function SchedulePage() {
+    return (
+        <div className={'p-10 w-full flex flex-col space-y-5 justify-center items-center'}>
+
+            <div className={'flex w-full items-center space-x-5'}>
+                <div className={'font-bold text-3xl'}>
+                    Data Jadwal Sekolah
+                </div>
+            </div>
+
+
+        </div>
+    )
+}
