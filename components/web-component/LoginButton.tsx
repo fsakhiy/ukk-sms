44f49f8dc2e5dd170bafdcd25348c7fc10a5ebf4
Fairuz Sakhiy <sakhiyfairuz@gmail.com>
2024-02-06 09:45:54 +0700

    changed button import

--- components/web-component/LoginButton.tsx
+++ components/web-component/LoginButton.tsx
@@ -1,5 +1,5 @@
 "use client"
 import { signIn } from "next-auth/react"
-import {Button} from "@/components/ui/Button";
+import {Button} from "@/components/ui/button";
 
 export default () => <Button onClick={() => signIn()}>Sign in</Button>
