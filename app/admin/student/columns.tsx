194454d307f5e2f3b87212635818aa4c6c0707c7
Fairuz Sakhiy <sakhiyfairuz@gmail.com>
2024-02-06 13:48:59 +0700

    added column information for data table

---
+++ app/admin/student/columns.tsx
@@ -0,0 +1,62 @@
+"use client"
+
+import {ColumnDef} from "@tanstack/table-core";
+import {Checkbox} from "@/components/ui/checkbox";
+
+export type StudentDataTableType = {
+    id: number,
+    name: string,
+    classroom: string,
+    username: string,
+    createdBy: string,
+    createdAt: Date,
+}
+
+export const columns: ColumnDef<StudentDataTableType>[] = [
+    {
+        id: "select",
+        header: ({ table }) => (
+            <Checkbox
+                checked={
+                    table.getIsAllPageRowsSelected() ||
+                    (table.getIsSomePageRowsSelected() && "indeterminate")
+                }
+                onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+                aria-label="Select all"
+            />
+        ),
+        cell: ({ row }) => (
+            <Checkbox
+                checked={row.getIsSelected()}
+                onCheckedChange={(value) => row.toggleSelected(!!value)}
+                aria-label="Select row"
+            />
+        ),
+        enableSorting: false,
+        enableHiding: false,
+    },
+    {
+        accessorKey: "id",
+        header: "Id"
+    },
+    {
+        accessorKey: "name",
+        header: "Nama Siswa"
+    },
+    {
+        accessorKey: "classroom",
+        header: "Kelas Siswa"
+    },
+    {
+        accessorKey: "username",
+        header: "Username Akun Siswa"
+    },
+    {
+        accessorKey: "createdBy",
+        header: "Dibuat Oleh"
+    },
+    {
+        accessorKey: "createdAt",
+        header: "Waktu Pembuatan"
+    }
+]
