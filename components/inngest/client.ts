801bcad760bde598bb61f516d41fc9f69c77d0e4
Fairuz Sakhiy <sakhiyfairuz@gmail.com>
2024-02-15 13:49:05 +0700

    added new event and its type

--- components/inngest/client.ts
+++ components/inngest/client.ts
@@ -7,10 +7,24 @@ type TestEvent = {
     }
 }
 
-type Events = {
-    "test/test.event": TestEvent
+type ScheduleGeneration = {
+    data: {
+        period: {
+            startFrom: Date
+            endIn: Date
+        }
+        classDetail: {
+            id: number,
+            scheduleOrder: "MONDAY" | "TUESDAY" | "WEDNESDAY" | "THURSDAY" | "FRIDAY"
+        }[]
+    }
 }
 
-export const inngest = new Inngest({ id: "my-app", schemas: new EventSchemas().fromRecord<Events>() });
+type Events = {
+    "test/test.event": TestEvent
+    "admin/class.detail.create": ScheduleGeneration
+}
+
+export const inngest = new Inngest({ id: "school-manager", schemas: new EventSchemas().fromRecord<Events>() });
 
 
