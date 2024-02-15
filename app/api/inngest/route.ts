de28b0abca816d7be960ecb13424bc350fa933b1
Fairuz Sakhiy <sakhiyfairuz@gmail.com>
2024-02-15 13:45:37 +0700

    added schedule generator function

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -2,8 +2,9 @@
 import { serve } from "inngest/next";
 import { inngest } from "@/components/inngest/client";
 import {testInngestFunction} from "@/components/inngestFn/test";
+import {generateScheduleDetail} from "@/components/inngestFn/scheduleGeneration";
 
 export const { GET, POST, PUT } = serve({
     client: inngest,
-    functions: [testInngestFunction],
+    functions: [testInngestFunction, generateScheduleDetail],
 });
