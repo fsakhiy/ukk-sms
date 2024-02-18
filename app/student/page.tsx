08873aafb0a12a154bba069ded46793bc6b05b35
Fairuz Sakhiy <sakhiyfairuz@gmail.com>
2024-02-18 20:49:05 +0700

    added student initial layout that showed the daily presence

--- app/student/page.tsx
+++ app/student/page.tsx
@@ -2,12 +2,55 @@
 
 import {getServerSession} from "next-auth";
 import prisma from '@/components/db/prisma'
+import LoginButton from "@/components/web-component/LoginButton";
 
 export default async function StudentHomePage() {
 
-    return (
-        <div>
-            halo
-        </div>
-    )
+    const userData = await getServerSession()
+
+    if(userData) {
+        const userDataFromDB = await prisma.user.findUnique({
+            where: {
+                username: userData.user.name
+            },
+        })
+
+        const studentData = await prisma.student.findUnique({
+            where: {
+                // @ts-ignore
+                id: userDataFromDB.studentId
+            },
+            include: {
+                classroom: true
+            }
+        })
+
+        const presenceData = await prisma.studentPresence.findFirst({
+            where: {
+                effectiveDate: {
+                    equals: new Date()
+                },
+                // @ts-ignore
+                studentId: studentData.id
+            }
+        })
+
+        console.log(presenceData)
+
+        return (
+            <div className={'flex flex-col gap-5 p-10'}>
+                <h1 className={'font-bold text-3xl'}>{studentData.name} - {studentData.classroom.name}</h1>
+                <div>
+
+                    {presenceData ? presenceData.status : 'tidak ada kbm untuk hari ini'}
+                </div>
+            </div>
+        )
+    } else {
+        return (
+            <div>
+                <LoginButton />
+            </div>
+        )
+    }
 }
