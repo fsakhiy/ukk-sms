8ada0ca3070d493231857d9a8e7bde62ebcc4def
Fairuz Sakhiy <sakhiyfairuz@gmail.com>
2024-02-23 21:54:24 +0700

    added page to see student schedule

---
+++ app/student/schedule/columns.tsx
@@ -0,0 +1,52 @@
+"use client"
+
+import {ColumnDef} from "@tanstack/table-core";
+import {Checkbox} from "@/components/ui/checkbox";
+
+export type StudentScheduleDataType = {
+    id: number,
+    subject: string,
+    startTime: string,
+    endTime: string
+}
+
+export const columns: ColumnDef<StudentScheduleDataType>[] = [
+    // {
+    //     id: "select",
+    //     header: ({ table }) => (
+    //         <Checkbox
+    //             checked={
+    //                 table.getIsAllPageRowsSelected() ||
+    //                 (table.getIsSomePageRowsSelected() && "indeterminate")
+    //             }
+    //             onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+    //             aria-label="Select all"
+    //         />
+    //     ),
+    //     cell: ({ row }) => (
+    //         <Checkbox
+    //             checked={row.getIsSelected()}
+    //             onCheckedChange={(value) => row.toggleSelected(!!value)}
+    //             aria-label="Select row"
+    //         />
+    //     ),
+    //     enableSorting: false,
+    //     enableHiding: false,
+    // },
+    // {
+    //     accessorKey: "id",
+    //     header: "Id"
+    // },
+    {
+        accessorKey: "subject",
+        header: "Mata Pelajaran"
+    },
+    {
+        accessorKey: "startTime",
+        header: "Waktu Mulai"
+    },
+    {
+        accessorKey: "endTime",
+        header: "Waktu Selesai"
+    },
+]
