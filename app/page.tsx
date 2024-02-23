74f469878ed187b5346c329ac8b2b5173a455fda
Fairuz Sakhiy <sakhiyfairuz@gmail.com>
2024-02-23 21:57:59 +0700

    fixed so that not everyone can access the page

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,44 @@
+"use server"
+
 import LoginButton from "@/components/web-component/LoginButton";
 import LogoutButton from "@/components/web-component/LogoutButton";
+import {getServerSession} from "next-auth";
+import prisma from '@/components/db/prisma'
+import {redirect} from "next/navigation";
 
-export default function HomePage() {
-  return (
-      <div>
-        welcome to school manager, this website is currently in development..
-          {/*<Button >Sign in</Button>*/}
-          <LoginButton />
-          <LogoutButton />
-      </div>
+export default async function HomePage() {
+    const user = await getServerSession()
 
-  )
+    if(user) {
+        const userFromDB = await prisma.user.findUnique({
+            where: {
+                // @ts-ignore
+                username: user.user.name
+            }
+        })
+
+        if(userFromDB.isAdmin) {
+            redirect('/admin')
+        } else {
+
+              return (
+                  <div>
+                    welcome to school manager, this website is currently in development..
+                      {/*<Button >Sign in</Button>*/}
+                      <LoginButton />
+                      <LogoutButton text={'logout'} />
+                  </div>
+              )
+            }
+    } else {
+        return (
+            <div>
+                welcome to school manager, this website is currently in development..
+                {/*<Button >Sign in</Button>*/}
+                <LoginButton />
+                <LogoutButton text={'logout'} />
+            </div>
+        )
+    }
+
 }
