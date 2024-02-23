ec8d58a20d845aaf62d9fe948e518b00e8f200dc
Fairuz Sakhiy <sakhiyfairuz@gmail.com>
2024-02-23 21:56:25 +0700

    fixed the layout

--- components/ui/student/presence.tsx
+++ components/ui/student/presence.tsx
@@ -2,6 +2,8 @@
 
 import {Button} from "@/components/ui/button";
 import {parsePresenceStatus} from "@/components/sharedFunction/functions";
+import {redirect} from "next/navigation";
+import Link from "next/link";
 
 export interface StudenDailyPresenceData {
     studentId: number
@@ -13,20 +15,27 @@ export interface StudenDailyPresenceData {
 
 
 export default function StudentDailyPresence({studentId, name, status, presenceTime, handler}: StudenDailyPresenceData) {
+    const statusReal = parsePresenceStatus(status)
+
     return (
-        <div className={''}>
-            <h1>{name} - {parsePresenceStatus(status)}</h1>
+        <div className={'flex flex-col gap-10 w-full max-w-lg'}>
+            {/*<h1>{name} - {parsePresenceStatus(status)}</h1>*/}
+
 
             {(status === "NOT_YET") ?
-                <div>
+                <div className={'flex flex-col w-full gap-3'}>
+                    <p className={'text-center'}>Kamu Belum Melakukan Presensi</p>
                     <Button
-                        variant={'outline'}
+                        // variant={'outline'}
                         onClick={async () => {
                             await handler(studentId)
                         }}
-                    >Absen Dulu</Button>
+                    >Presensi Dulu Yuk</Button>
                 </div>
-                : <div>kamu sudah absen - {presenceTime?.toString()}</div> }
+                : <div>Kamu Sudah Absen - {parsePresenceStatus(status) === 'Terlambat' ? <p>Terlambat</p> : <p>{parsePresenceStatus(status)}</p>} <br/> {presenceTime?.toString()} </div>
+            }
+
+            <Button className={'w-full'} variant={'outline'} ><Link href={'/student/history'}>riwayat presensi</Link></Button>
         </div>
     )
 }
