fe010f35c0af10772e76e27770fc3437cf2733a4
Fairuz Sakhiy <sakhiyfairuz@gmail.com>
2024-02-23 22:24:40 +0700

    revamped the ui

--- components/ui/student/classInformation.tsx
+++ components/ui/student/classInformation.tsx
@@ -1,18 +1,32 @@
+"use client"
+
 import {Button} from "@/components/ui/button";
+import {logToClass} from "@/app/student/class/action";
+import {toast} from "@/components/ui/use-toast";
+import {Toaster} from "@/components/ui/toaster";
+import {parsePresenceStatus} from "@/components/sharedFunction/functions";
 
 interface classInfoType {
     id: number,
+    classId: number,
     name: string,
-    // handler: (id: number) => Promise<void>
     startFrom: Date,
     endIn: Date,
     teacher: string
-    presenceStatus: string
+    presenceStatus: "NOT_YET" | "LATE" | "ON_TIME"
 }
 
-export default function ClassInformation({id, name, startFrom, endIn, teacher, presenceStatus }: classInfoType) {
+export default function ClassInformation({id, classId, name, startFrom, endIn, teacher, presenceStatus }: classInfoType) {
+
+    const handlePresence = async () => {
+        await logToClass(id, classId)
+        toast({description: 'masuk kelas sukses'})
+    }
+
+
     return (
         <div className={'p-2 border rounded-lg flex flex-col md:items-center md:flex-row gap-5'}>
+            <Toaster />
             <div>
                 <p className={'font-bold text-2xl'}>
                     {name} - {teacher}
@@ -21,13 +35,19 @@ export default function ClassInformation({id, name, startFrom, endIn, teacher, p
                    {startFrom.toTimeString().split(' ')[0]} - {endIn.toTimeString().split(' ')[0]}
                 </p>
                 <p className={'underline font-semibold'}>
-                    {presenceStatus}
+                    {parsePresenceStatus(presenceStatus)}
                 </p>
             </div>
 
-            <Button className={'md:ml-auto'}>
+            {presenceStatus != "NOT_YET" ?
+            <div className={'text-center font-bold'}>kamu sudah absen</div>
+                :
+            <Button
+                onClick={handlePresence}
+                className={'md:ml-auto'}>
                 Masuk kelas
             </Button>
+            }
         </div>
     )
 }
