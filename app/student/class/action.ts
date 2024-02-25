c8d7ed886c8fb8ef4274a83ac7cdd8acf451672a
Fairuz Sakhiy <sakhiyfairuz@gmail.com>
2024-02-25 16:31:41 +0700

    added new logToClass that check class code (can i use the old one? yes, idk why i make a new one)

--- app/student/class/action.ts
+++ app/student/class/action.ts
@@ -17,4 +17,34 @@ const logToClass = async (studentId: number, classId: number) => {
     revalidatePath('/student/class')
 }
 
-export { logToClass }
+const logToClassNew = async (studentId: number, classId: number, classCode: string) => {
+    const classDetail = await prisma.studentClassPresence.findUnique({
+        where: {
+            id: classId
+        },
+        include: {
+            classes: true
+        }
+    })
+
+    console.log(classDetail?.classes.classCode)
+
+    if(classCode != classDetail?.classes.classCode) {
+        throw new Error(`kode ${classCode} salah`)
+    }
+
+    const presenceData = await prisma.studentClassPresence.update({
+        where: {
+            id: classId
+        },
+        data: {
+            logTime: new Date(),
+            status: 'ON_TIME'
+        }
+    })
+
+    revalidatePath('/student/class')
+
+}
+
+export { logToClass, logToClassNew }
