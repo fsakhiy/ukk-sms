233699b10436f17f67db01489cd0a6cb3bd4b94f
Fairuz Sakhiy <sakhiyfairuz@gmail.com>
2024-02-27 13:45:34 +0700

    added action to update student user data

---
+++ app/student/setting/action.ts
@@ -0,0 +1,23 @@
+"use server"
+
+import prisma from '@/components/db/prisma'
+import * as bcrypt from "bcrypt";
+import {revalidatePath} from "next/cache";
+import {z} from "zod";
+import {formSchema} from "@/components/ui/student/studentAccountSettings";
+
+async function updateUserInformation(userId: number, values: z.infer<typeof formSchema>) {
+    const userData = await prisma.user.update({
+        where: {
+            id: userId
+        },
+        data: {
+            username: values.username,
+            password: await bcrypt.hash(values.password, 12)
+        }
+    })
+
+    revalidatePath('/student/setting')
+}
+
+export { updateUserInformation }
