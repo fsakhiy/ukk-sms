2e80938ff7f28ff4804b2874c7ecaa6b49f78109
Fairuz Sakhiy <sakhiyfairuz@gmail.com>
2024-02-27 16:23:59 +0700

    fixed the styling and added autoredirect for people who have logged in

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import LogoutButton from "@/components/web-component/LogoutButton";
 import {getServerSession} from "next-auth";
 import prisma from '@/components/db/prisma'
 import {redirect} from "next/navigation";
+import Link from "next/link";
 
 export default async function HomePage() {
     const user = await getServerSession()
@@ -17,26 +18,36 @@ export default async function HomePage() {
             }
         })
 
-        if(userFromDB.isAdmin) {
+        if(userFromDB?.isAdmin) {
             redirect('/admin')
+        } else if(userFromDB?.accountType === "STUDENT") {
+            redirect('/student')
+        } else if(userFromDB?.accountType === "TEACHER") {
+            redirect('/teacher')
         } else {
 
               return (
-                  <div>
-                    welcome to school manager, this website is currently in development..
-                      {/*<Button >Sign in</Button>*/}
-                      <LoginButton />
-                      <LogoutButton text={'logout'} />
+                  <div className={'p-5 lg:p-10 flex flex-col gap-10'}>
+                      <h1 className={'font-bold text-3xl'}>Selamat datang di School Manager</h1>
+                      <div className={'flex flex-col sm:flex-row sm:items-center gap-5'}>
+                          <p>
+                              silahkan login untuk mengakses aplikasi ini
+                          </p>
+                          <LoginButton/>
+                      </div>
                   </div>
               )
-            }
+        }
     } else {
         return (
-            <div>
-                welcome to school manager, this website is currently in development..
-                {/*<Button >Sign in</Button>*/}
-                <LoginButton />
-                <LogoutButton text={'logout'} />
+            <div className={'p-5 lg:p-10 flex flex-col gap-10'}>
+                <h1 className={'font-bold text-3xl'}>Selamat datang di School Manager</h1>
+                <div className={'flex flex-col sm:flex-row sm:items-center gap-5'}>
+                    <p>
+                        silahkan login untuk mengakses aplikasi ini
+                    </p>
+                    <LoginButton/>
+                </div>
             </div>
         )
     }
