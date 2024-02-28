af1129ba2de3127ce37b65f785efbfe38f4d031c
Fairuz Sakhiy <sakhiyfairuz@gmail.com>
2024-02-28 14:10:29 +0700

    added auto close when a link is clicked

--- components/ui/student/NavBar.tsx
+++ components/ui/student/NavBar.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import {
     Sheet,
     SheetContent,
@@ -10,9 +12,7 @@ import {Button} from "@/components/ui/button";
 import Link from "next/link";
 import {Menu} from "lucide-react";
 import LogoutButton from "@/components/web-component/LogoutButton";
-
-
-
+import React from "react";
 
 interface studentNavbar {
     name: string,
@@ -20,10 +20,12 @@ interface studentNavbar {
 }
 
 export default function StudentNavbar({name, signOutText}: studentNavbar) {
+    const [open, setOpen] = React.useState(false);
+
     return (
         <div>
             {/*Selamat Datang, {name}!*/}
-            <Sheet>
+            <Sheet open={open} onOpenChange={setOpen}>
                 <SheetTrigger className={'flex gap-2'}>
                     {/*<Button>Menu</Button>*/}
                     <Menu /> Menu
@@ -37,29 +39,44 @@ export default function StudentNavbar({name, signOutText}: studentNavbar) {
                     </SheetHeader>
                     <div className={'w-full flex flex-col gap-3 mt-10'}>
                         <Link href={'/student'}>
-                            <Button variant={'outline'} className={'w-full'}>
+                            <Button
+                                variant={'outline'}
+                                onClick={() => {setOpen(false)}}
+                                className={'w-full'}>
                                 Presensi Hari Ini
                             </Button>
                         </Link>
                         <Link href={'/student/history'}>
-                            <Button variant={'outline'} className={'w-full'}>
+                            <Button
+                                variant={'outline'}
+                                onClick={() => {setOpen(false)}}
+                                className={'w-full'}>
                                 Riwayat Presensi Harian
                             </Button>
                         </Link>
 
                         <Link href={'/student/class'}>
-                            <Button variant={'outline'} className={'w-full'}>
+                            <Button
+                                variant={'outline'}
+                                onClick={() => {setOpen(false)}}
+                                className={'w-full'}>
                                 Presensi Jam KBM
                             </Button>
                         </Link>
                         <Link href={'/student/schedule'}>
-                            <Button  variant={'outline'} className={'w-full'}>
+                            <Button
+                                variant={'outline'}
+                                onClick={() => {setOpen(false)}}
+                                className={'w-full'}>
                                 Jadwal Pembelajaran
                             </Button>
                         </Link>
 
                         <Link href={'/student/setting'}>
-                            <Button  className={'w-full'}>
+                            <Button
+                                className={'w-full'}
+                                onClick={() => {setOpen(false)}}
+                            >
                                 Pengaturan Akun
                             </Button>
                         </Link>
